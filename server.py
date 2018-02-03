695c6853c0c72c7a4f992d78a9d3d78179ed3f57
bcabio <briancabigon@gmail.com>
2018-02-03 02:46:00 -0500

    Added initial server with test routes

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient('mongodb://admin2:admin2@ds121898.mlab.com:21898/mangohacks')
+db = client['mangohacks']
+
+test = db['test']
+
+test_doc = {
+	"author": "aimee"
+}
+
+@app.route("/")
+def hello():
+	name = test.find_one()
+	return jsonify(str(name))
+
+@app.route("/insert")
+def insert():
+	doc_cursor = test.insert_one(test_doc)
+	doc_id = doc_cursor.inserted_id
+	print(doc_cursor)
+	print(doc_id)
+	return "good"
